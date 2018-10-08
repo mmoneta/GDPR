a08fe6d8c5f370fa1fd7b58722b93391abb5b08d
mmoneta <jjaymatt@gmail.com>
2018-10-08 22:03:31 +0200

    init

--- plugin/index.js
+++ plugin/index.js
@@ -6,8 +6,8 @@ class plugin {
   
   setCookie(cname, cvalue) {
     var d = new Date();
-    d.setTime(d.getTime() + d.getTimezoneOffset() + (24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toString();
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
   
